c23c0f8d50e6c2f33bac4a5a92a602e83244f249
Håkon Flatval <hkon20@hotmail.com>
2020-12-12 20:53:27 +0100

    Update test.cpp

--- examples/test.cpp
+++ examples/test.cpp
@@ -8,6 +8,26 @@
 
 namespace gn = generelle;
 
+void drawModel(const gn::GE model, int width, int height, const std::string& file_name) {
+
+    unsigned char* ch = gn::Visualization::visualize(model, width, height);
+
+    std::unique_ptr<OIIO::ImageOutput> outfile = OIIO::ImageOutput::create(file_name);
+    if (!outfile) {
+        std::cerr << "Cannot open output file " << file_name << ", exiting" << std::endl;
+        exit(-1);
+    }
+
+    OIIO::ImageSpec spec(width, height, 4, OIIO::TypeDesc::UINT8);
+    outfile->open(file_name, spec);
+    outfile->write_image(OIIO::TypeDesc::UINT8, ch);
+    outfile->close();
+
+    gn::Visualization::destroyBuffer(ch);
+
+    std::cout << "Wrote image to " << file_name << std::endl;
+}
+
 int main() {
     const int width = 800;
     const int height = 600;
@@ -18,23 +38,18 @@ int main() {
     gn::GE sum = ge1.add(ge2); */
 
     gn::GE box = gn::makeBox(falg::Vec3(2.0f, 1.0f, 1.0f));
+    gn::GE sphere = gn::makeSphere(0.5f).translate(falg::Vec3(0.0f, 1.5f, 0.0f));
 
-    unsigned char* ch = gn::Visualization::visualize(box, width, height);
+    gn::GE sum = box.smoothAdd(sphere, 0.5f).pad(1.0f).subtract(gn::makeSphere(2.f).translate(falg::Vec3(0.5f, 1.5f, 0.0f)));
 
-    std::string output_file_name = "out2.png";
-    
-    OIIO::ImageOutput *outfile = OIIO::ImageOutput::create(output_file_name);
-    if (!outfile) {
-        std::cerr << "Cannot open output file " << output_file_name << ", exiting" << std::endl;
-        exit(-1);
-    }
+    // gn::GE tot = sum.scale(0.6f);
 
-    OIIO::ImageSpec spec(width, height, 4, OIIO::TypeDesc::UINT8);
-    outfile->open(output_file_name, spec);
-    outfile->write_image(OIIO::TypeDesc::UINT8, ch);
-    outfile->close();
-    
-    gn::Visualization::destroyBuffer(ch);
+    // gn::GE tot = gn::makeSphere(0.5f).scale(0.9f).translate(falg::Vec3(0.0f, 0.0f, 0.0f));
+    // gn::GE tot2 = gn::makeSphere(0.5f).scale(0.9f).translate(falg::Vec3(0.0f, 1.0f, 2.0f));
 
-    std::cout << "Wrote image to " << output_file_name << std::endl;
+    gn::GE tot = gn::makeBox(falg::Vec3(1.0f, 1.0f, 1.0f)).scale(1.0f);
+    gn::GE tot2 = gn::makeBox(falg::Vec3(1.0f, 1.0f, 1.0f)).scale(0.8f);
+
+    drawModel(tot, width, height, "out.png");
+    drawModel(tot2, width, height, "out2.png");
 }
